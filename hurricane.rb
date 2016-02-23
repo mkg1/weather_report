c39be2a0df423af7f3610eb42fef0738ba5dd1bb
Molly G <gehring.mg@gmail.com>
2016-02-22 19:39:13 -0500

    Add hurricane list method

--- hurricane.rb
+++ hurricane.rb
@@ -1,9 +1,18 @@
+require 'json'
+
 class Hurricane
-  def initialize
+  def initialize(location)
+    @response = JSON.parse(File.read("hurricanes.json"))
   end
-#test
-  # def current_hurricane_list
-  #   array = @response["currenthurricane"]
-  #   puts array
-  # end
+
+  def current_hurricane_list
+    storms = @response["currenthurricane"]
+    storms.each do |s|
+        name = s["stormInfo"]["stormName_Nice"]
+        windspeed = s["Current"]["WindSpeed"]["Mph"]
+        direction_angle = s["Current"]["Movement"]["Degrees"]
+        direction = s["Current"]["Movement"]["Text"]
+        puts "Currently, #{name} is moving at #{windspeed} miles per hour and heading #{direction_angle} #{direction}."
+      end
+  end
 end
